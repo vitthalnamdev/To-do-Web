1e7e38ce54fe05f905bc39e2d4d341b4e2d04a9f
Vitthal Namdev <vitthal7804@gmail.com>
2025-02-22 20:12:14 +0530

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,28 +7,28 @@ import "./App.css";
 
 const HOST = "https://to-do-web-backend.onrender.com";
 
- 
+const App = () => {
     const Delete = async (id) => {
         try {
-            // Use .filter() instead of a for loop
-            const temp = todos.filter((item) => item._id !== id);
+            let temp = [];
+            for (let item in todos) {
+                if (todos[item]._id !== id) {
+                    temp.push(todos[item]);
+                }
+            }
             setTodos(temp);
-
-            // Wait for the DELETE request to complete
             const response = await fetch(`${HOST}/data/${id}`, {
                 method: 'DELETE',
             });
-
-            // Check if the response is successful
-            if (!response.ok) {
-                throw new Error(`Failed to delete item: ${response.statusText}`);
+            if (response.ok){  
+               // do nothing.
+            } else {
+                console.log("Error in deleting");
             }
-
         } catch (err) {
-            console.error('Error deleting item:', err);
+            console.error('Error updating item:', err);
         }
     };
- 
 
      
 
